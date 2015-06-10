e98bac4d6540380989d5a4933fdd37566d6a4700
Mario Naranjo <mario15695@hotmail.com>
2015-06-10 11:44:29 -0600

    Versión 5

--- XMLTest/Pruebas_XML_Facturas.sql
+++ XMLTest/Pruebas_XML_Facturas.sql
@@ -1,13 +1,20 @@
-
-SELECT EXTRACTVALUE(VALUE(d), '/NombreDeArticulo') "Nombre del Articulo"
-FROM "Facturas_XML",
-      TABLE(XMLSEQUENCE(EXTRACT(OBJECT_VALUE, '/Factura/LineasDeCompra/LineaDeArticulo/NombreDeArticulo'))) d
+--Obtener la descripción de las líneas de una factura
+SELECT EXTRACTVALUE(VALUE(descripcion), '/NombreDeArticulo') "Nombre del Articulo"
+FROM "Facturas_XML", TABLE(XMLSEQUENCE(EXTRACT(OBJECT_VALUE, '/Factura/LineasDeCompra/LineaDeArticulo/NombreDeArticulo'))) descripcion
 WHERE EXISTSNODE
   (
     OBJECT_VALUE,
-    '/Factura[ CodigoFactura = "Fact01"]'
+    '/Factura[ CodigoFactura = "Fact02"]'
   )=1;
+  
+
+--Extraer los datos de todas las facturas
+SELECT EXTRACTVALUE(OBJECT_VALUE, '/Factura/CodigoFactura'),
+       EXTRACTVALUE(VALUE(linea), '/LineaDeArticulo/@numeroLinea'),
+       EXTRACTVALUE(VALUE(linea), 'LineaDeArticulo/ArticuloComprado/@codigo'),
+       EXTRACTVALUE(VALUE(linea), 'LineaDeArticulo/ArticuloComprado/@cantidad'),
+       EXTRACTVALUE(VALUE(linea), 'LineaDeArticulo/ArticuloComprado/@precioUnitario')
+FROM "Facturas_XML", TABLE(XMLSEQUENCE(EXTRACT(OBJECT_VALUE, 'Factura/LineasDeCompra/LineaDeArticulo'))) linea;
 
 
     
-SELECT * FROM "Facturas_XML";
