062c31280b724aa0495268ab1f4647853953bfce
Mario Naranjo <mario15695@hotmail.com>
2015-06-12 13:58:49 -0600

    Versión 6

--- XMLTest/Pruebas_XML_Procedimiento.sql
+++ XMLTest/Pruebas_XML_Procedimiento.sql
@@ -1,4 +1,5 @@
 
+--Obtiene e imprime las facturas XML del procedimiento sp_mostrarFacturasXML a través de un cursor REFCURSOR (SYS_REFCURSOR)
 VARIABLE facturas REFCURSOR
 EXECUTE SP_MOSTRARFACTURASXML(:facturas);
 PRINT facturas;
--- XMLTest/XMLFormat.sql
+++ XMLTest/XMLFormat.sql
@@ -1,7 +1,9 @@
 
+--Selecciona las facturas XML de la vista FACTURAS_XML_VIEW dándoles el formato XML Ccon XMLROOT
 SELECT XMLROOT(x.OBJECT_VALUE, VERSION '1.0', STANDALONE YES) FROM FACTURAS_XML_VIEW x;
 
 
+--Imprime las facturas de la vista: FACTURAS_XML_VIEW obtenidas dentro de un cursor
 DECLARE
   xml XMLTYPE;
   
@@ -19,13 +21,6 @@ BEGIN
   END LOOP;
   
   CLOSE XMLCursor;
-END;
-
-
-VARIABLE n NUMBER
-BEGIN
-:n := 1;
-print n;
 END;
 
 
