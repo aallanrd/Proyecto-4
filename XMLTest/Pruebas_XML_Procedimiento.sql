24c04246415c080523bc6c7b539c8f1dfab74bed
Mario Naranjo <marionaranjo12@gmail.com>
2015-06-17 09:37:19 -0600

    Versión 7.1

--- XMLTest/Pruebas_XML_Procedimiento.sql
+++ XMLTest/Pruebas_XML_Procedimiento.sql
@@ -5,7 +5,6 @@ EXECUTE SP_CREARVISTAFACTURA_XML;
 SELECT XMLROOT(x.OBJECT_VALUE, VERSION '1.0', STANDALONE YES) "Facturas" FROM FACTURAS_XML_VIEW x;
 
 
-
 --Obtiene y muestra el contenido de las tablas de facturas relacionales****************************************************
 VARIABLE facturas REFCURSOR
 EXECUTE SP_MOSTRARFACTURAS(:facturas);
