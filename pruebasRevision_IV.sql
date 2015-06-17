d8eeeb074c7791654de27450d5c369c3bd9c510b
Mario Naranjo <marionaranjo12@gmail.com>
2015-06-17 09:45:33 -0600

    Versión 7.1

--- pruebasRevision_IV.sql
+++ pruebasRevision_IV.sql
@@ -3,6 +3,9 @@ delete facturas_obj;
 commit;
 select * from facturas_obj;
 
+SELECT f.codigoFactura, f.fechaCompra, lineas.* 
+FROM Facturas_OBJ f, TABLE(f.lineasDeCompra) lineas;
+
 
 --PRUEBAS DE LAS VISTA
 SELECT * FROM FACTURA_VIEW;
