5e3cdf0936394c2d75d723fe494f14616b5bb9e0
Mario Naranjo <marionaranjo12@gmail.com>
2015-06-17 09:09:10 -0600

    Versión 7

--- XMLTest/Pruebas_XML_Procedimiento.sql
+++ XMLTest/Pruebas_XML_Procedimiento.sql
@@ -1,8 +1,15 @@
---Procedimiento para generación de la vista
+--Procedimiento para generación de la vista*********************************************************************************
 EXECUTE SP_CREARVISTAFACTURA_XML;
 
 --Selecciona las facturas XML de la vista FACTURAS_XML_VIEW dándoles el formato XML con XMLROOT
 SELECT XMLROOT(x.OBJECT_VALUE, VERSION '1.0', STANDALONE YES) FROM FACTURAS_XML_VIEW x;
+
+
+
+--Obtiene y muestra el contenido de las tablas de facturas relacionales****************************************************
+VARIABLE facturas REFCURSOR
+EXECUTE SP_MOSTRARFACTURAS(:facturas);
+PRINT facturas;
 
 
 
@@ -11,8 +18,3 @@ VARIABLE facturas REFCURSOR
 EXECUTE SP_MOSTRARFACTURASXML(:facturas);
 PRINT facturas;
 
-
---Obtiene y muestra el contenido de las tablas de facturas relacionales
-VARIABLE facturas REFCURSOR
-EXECUTE SP_MOSTRARFACTURAS(:facturas);
-PRINT facturas;
